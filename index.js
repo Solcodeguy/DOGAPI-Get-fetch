845eaa64fd1e42b17ed16998ed43b88ea79b15bb
solcodeguy <smeighn@gmail.com>
2019-10-12 17:13:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,41 @@
 
 function getDogImages(numImg) {
 
-    if (numImg <= 0 || numImg > 50) {
-        throw "Image not found"
-    };
+    if (numImg.length === 0) {
 
-    fetch(`https://dog.ceo/api/breeds/image/random/${numImg}`).then(response => {
-        response.json()
-        displayResults(responseJson, numImg)
-    }).catch(error => {
-        alert('Something went wrong. Try again later.')
-    });
+        let userInput = 3;
+        dogImgPull(userInput);
+
+    } else if (numImg > 50) {
+        alert('Pick a number between 1 - 50');
+
+    } else if (numImg < 1) {
+        alert('Pick a number between 1 - 50');
+
+    } else {
+
+        let userInput = numImg;
+        dogImgPull(userInput);
+    }
+}
+
+function dogImgPull(userInput) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${numImg}`)
+        .then(response => response.json())
+
+    .then(responseJson =>
+
+            displayResults(responseJson, numImg))
+        .catch(error => alert('Something went wrong. Try again'));
 }
 
 function displayResults(responseJson, numImg) {
     console.log(responseJson.message);
     //replace the existing image with the new one(s)
-    $('.results').append(`<h2>Dog images:</h2>`);
-    for (let i = 0; i < numImg; i++) {
-        $('.results').append(`<img src="${responseJson.message[i]}" class="results-img">`);
-    }
+    for (let i = 0; i <
+        responseJson.message.length; i++) {
+        $('.results').append(`<img src="${responseJson.message[i]}">`);
+    };
     //display the results section
     $('.results').removeClass('hidden');
 }
